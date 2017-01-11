e6f40517610d6d51f9213ddb5ae6f62972e93ac6
Aleksandr <hiphopkrv@gmail.com>
2017-01-11 16:14:31 +0200

    working getLocalWeather

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,51 @@
 ﻿var app = angular.module('LocalWeather', []);
 
-app.factory('localWeatherService', [
-    '$http', function ($http) {
-        var apiKey = 'ac6ddc9daa54317b3f571c9238e0006a';
-        var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?';
-        var latitude = 0;
-        var longitude = 0;
-
-        function getCoords() {
-            if (window.navigator.geolocation) {
-                window.navigator.geolocation.getCurrentPosition(function (position) {
-                    latitude = position.coords.latitude;
-                    longitude = position.coords.longitude;
-                });
-            }
-        }
-
-        function formUrl() {
-            return apiUrl += 'lat=' + latitude + "&lon=" + longitude + "&APPID=" + apiKey;
-        }
-
-        return {
-            getLocalWeather: function (successCallback) {
-                getCoords();
-                var url = formUrl();
-                $http.get(url).success(successCallback);
-            }
-        }
-    }
-]);
+var apiKey = 'ac6ddc9daa54317b3f571c9238e0006a';
+var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?';
 
 app.controller('LocalWeatherController', [
-    '$scope', 'localWeatherService', function ($scope, localWeatherService) {
-        $scope.weatherObj = {};
-
+    '$scope', '$http', '$window', function ($scope, $http, $window) {
         $scope.temperature = 0;
+        $scope.humidity = 0;
+        $scope.pressure = 0;
+        
         $scope.city = '';
-        $scope.country = '';
 
-        localWeatherService.getLocalWeather(onSuccess);
+        $scope.weatherDescription = '';
+        $scope.icon = '';
 
-        function onSuccess(data) {
-            $scope.weatherObj = data;
+        $scope.latitude = 48.464717;
+        $scope.longitude = 35.046183;
+
+        $scope.getLocalWeather = function () {
+            if ($window.navigator.geolocation) {
+                $window.navigator.geolocation.getCurrentPosition(function (position) {
+                    $scope.latitude = position.coords.latitude;
+                    $scope.longitude = position.coords.longitude;
+                });
+            }
+            var url = createUrl($scope.latitude, $scope.longitude);
+            $http.get(url).then(parseData, onError);
+        }
+
+        $scope.toggleTemperature = function() {
+            
+        }
+
+        function parseData(response) {
+            $scope.city = response.data.name;
+            $scope.temperature = response.data.main.temp;
+            $scope.humidity = response.data.main.humidity;
+            $scope.pressure = response.data.main.pressure;
+            $scope.weatherDescription = response.data.weather[0].description;
+        }
+
+        function onError(errorData) {
+            console.log(errorData);
+        }
+
+        function createUrl(latitude, longitude) {
+            return  apiUrl + 'lat=' + latitude + "&lon=" + longitude + "&APPID=" + apiKey;
         }
     }
 ]);
